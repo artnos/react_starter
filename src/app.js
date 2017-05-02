7a2d9cf548938dfc610f15e6b8ab64b3749b7bef
Artthapong Siriamonthep <asiriamonthep@crf.org>
2017-05-02 16:30:45 -0400

    Add Class with ES6

--- src/app.js
+++ src/app.js
@@ -2,11 +2,14 @@ const css = require('./scss/app.scss');
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import MyClass from './components/MyClass';
+
 
 ReactDOM.render(
   <div>
   	<h1>Hello, world!</h1>
   	<p>Some Copy</p>
+  	<MyClass/>
   </div>,
   document.getElementById('root')
 )
