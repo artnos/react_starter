59858ccfa340ebba743ec0e02ffcf718790aafc8
Artthapong Siriamonthep <asiriamonthep@crf.org>
2017-10-06 16:49:45 -0400

    write todo for updating react starter

--- src/App.js
+++ src/App.js
@@ -5,6 +5,16 @@ import ReactDOM from 'react-dom';
 import MyClass from './components/MyClass';
 
 
+/*
+* TODO: add redux and connect
+* TODO: add redux thunk middleware
+* TODO: add redux promise middleware
+* TODO: add redux tool kit and hot reloader
+* TODO: aciton and reducer folders
+* TODO: and bootsrap render
+* TODO: update react version
+*
+* */
 ReactDOM.render(
   <div>
   	<h1>Hello, world!</h1>
